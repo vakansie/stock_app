dabc89119c1b302ba4bcc804984eddb3c58033a3
vakansie <91331489+vakansie@users.noreply.github.com>
2024-04-20 17:00:22 +0200

    Add files via upload

--- stock_app.py
+++ stock_app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 import webbrowser
+import socket
+
+ALLOWED_TABLES = {'growkits', 'spores'}
 
 app = Flask(__name__)
 
@@ -12,38 +15,47 @@ def get_db_connection():
 @app.route('/')
 def inventory():
     conn = get_db_connection()
+    # Fetching growkits
     products_raw = conn.execute('SELECT * FROM growkits ORDER BY name, size').fetchall()
-    conn.close()
-    # Grouping products by name
     products_grouped = {}
     for product in products_raw:
         if product['name'] in products_grouped:
             products_grouped[product['name']].append(product)
         else:
             products_grouped[product['name']] = [product]
-    return render_template('inventory.html', products_grouped=products_grouped)
+    # Fetching spores
+    spores_raw = conn.execute('SELECT * FROM spores ORDER BY name, form').fetchall()
+    spores_grouped = {}
+    for spore in spores_raw:
+        if spore['name'] in spores_grouped:
+            spores_grouped[spore['name']].append(spore)
+        else:
+            spores_grouped[spore['name']] = [spore]
+    conn.close()
+    return render_template('inventory.html', products_grouped=products_grouped, spores_grouped=spores_grouped)
 
-@app.route('/update_stock/<int:id>', methods=['POST'])
-def update_stock(id):
-    # calculate the difference between what the user thinks the stock value is and what the user says it should be. This allows for concurrent editing.
+@app.route('/update_stock/<table>/<int:id>', methods=["POST"])
+def update_stock(table, id):
+    if table not in ALLOWED_TABLES:
+        return "Invalid table specified.", 400
+    # the difference between what the user thinks the stock value is and what the user says it should be. This allows for concurrent editing.
     last_refresh_stock = int(request.form['last_refresh_stock'])
-    submitted_stock = int(request.form['submitted_stock'])
+    submitted_stock = int(request.form['submitted_stock']) if 'submitted_stock' in request.form and request.form['submitted_stock'] else 0
     stock_difference = submitted_stock - last_refresh_stock
-    if stock_difference == 0: return redirect('/')
-    # Update the stock in the database using the difference
     conn = get_db_connection()
-    conn.execute('UPDATE growkits SET stock = stock + ? WHERE id = ?', (stock_difference, id))
+    query = 'UPDATE {} SET stock = stock + ? WHERE id = ?'.format(table)
+    conn.execute(query, (stock_difference, id))
     conn.commit()
     conn.close()
     return redirect('/')
 
 def main():
     from waitress import serve
-    # replace ip with your machines ipv4 to run on your local network
-    ip = '127.0.0.1'
-    print(f'\nrunning on http://{ip}:8080/\n')
-    webbrowser.open(f'http://{ip}:8080/', new=2)
-    serve(app, host=ip, port=8080)
+    hostname = socket.gethostname()
+    ipv4_address = socket.gethostbyname(hostname)
+    print(f'\nrunning on http://{ipv4_address}:8080/\n')
+    webbrowser.open(f'http://{ipv4_address}:8080/', new=2)
+    serve(app, host=f'{ipv4_address}', port=8080)
 
 if __name__ == '__main__':
     main()
